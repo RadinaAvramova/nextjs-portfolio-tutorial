a38c0cd09d92e19ffd51a9418c682ca76f390a76
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-06-30 12:53:23 +0300

    Update header.tsx

--- portfolio-site/components/header.tsx
+++ portfolio-site/components/header.tsx
@@ -1,11 +1,11 @@
 const Header = () => {
   return (
     <div className="text-center border-b border-slate-300 p-4 text-gray-500">
-      <div className="text-2xl font-bold text-slate-700">John Smith</div>
+      <div className="text-2xl font-bold text-slate-700">Radina Avramova</div>
       <div className="">Software Developer</div>
       <div className="flex justify-center gap-2 text-sm">
-        <div>M: 045006000</div>
-        <div>E: john@software.com</div>
+        <div>M: </div>
+        <div>E: radina@software.com</div>
       </div>
     </div>
   );
